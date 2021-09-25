7ba62ba8e32064e20b7985d17bc1a252038f216f
yurik002 <90479612+YuRiK002@users.noreply.github.com>
2021-09-25 19:38:35 +0300

    first

---
+++ js/bike.js
@@ -0,0 +1,50 @@
+const slides = document.querySelectorAll('.slide'),
+      dots = document.querySelectorAll('.dot');
+
+let index = 0;
+
+const activeSlide = n => {
+    for(slide of slides) {
+        slide.classList.remove('active');
+    }
+    slides[n].classList.add('active');
+}
+
+const activeDot = n => {
+    for(dot of dots) {
+        dot.classList.remove('active');
+    }
+    dots[n].classList.add('active');
+}
+
+const prepareCurrentSlide = ind => {
+    activeSlide(index);
+    activeDot(index);
+}
+
+const nextSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    }else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+const prevSlide = () => {
+    if(index == slides.length - 1) {
+        index = 0;
+        prepareCurrentSlide(index);
+    }else {
+        index++;
+        prepareCurrentSlide(index);
+    }
+}
+
+dots.forEach((item, indexDot)=> {
+    item.addEventListener('click', () => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+    })
+})
